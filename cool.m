00b6f51ec342030d9b703d76f0379ffd9ab1c3f0
NoahR64 <nroth8@asu.edu>
2024-10-30 15:16:39 -0700

    Add files via upload

--- cool.m
+++ cool.m
@@ -1,88 +1,75 @@
 brick = ConnectBrick('GROUP8');
 
-ultra = 'C';
+ultra = 1;
 leftMot = 'D';
 rightMot = 'A';
 colSen = 3;
 gyro = 4;
+moveSpeed = 50;
+moveTime = 1.36;
+turnSpeed = 75;
+turnTime = 0.432;
 
 brick.SetColorMode(colSen, 4);
 
-pickup = true;
-while pickup
-
-    disp(pickup);
-    pickup = false;
-end
-
 driving = true;
 while driving
-    rgb = brick.ColorRGB(colSen);
+    pause(0.5);
+
+    %determine the direction to go
     dist = brick.UltrasonicDist(ultra);
-    brick.GyroCalibrate(gyro);
-    disp(dist);
-    disp(brick.GyroAngle(gyro))
-    disp(angle);
-    pause(1);
-        if dist > 1
-            brick.MoveMotor(leftMot+rightMot, 50);
-            pause(2);
-        else
-            angle = brick.GyroAngle(gyro);
-            while abs(brick.GyroAngle(gyro))<abs(angle)+90
-                brick.MoveMotor(rightMot, 25);
-                brick.MoveMotor(leftMot, -25);
-            end
+    if dist < 30 %if forward is impossible, look right
+        %rotate right
+            brick.MoveMotor(leftMot, turnSpeed);
+            brick.MoveMotor(rightMot, -turnSpeed);
+            pause(turnTime);
             brick.MoveMotor(leftMot+rightMot, 0);
-            dist = brick.UltrasonicDist(1);
-            if dist > 1
-                brick.MoveMotor(leftMot+rightMot, 50);
-                pause(2);
-            else
-                angle = brick.GyroAngle(gyro);
-                while abs(brick.GyroAngle(gyro))<abs(angle)+90
-                brick.MoveMotor(leftMot, 25);
-                brick.MoveMotor(rightMot, -25);
-                end
+        dist = brick.UltrasonicDist(ultra);
+        if dist < 30 %if right is impossible, look left
+            %rotate left x2
+                brick.MoveMotor(leftMot, -turnSpeed);
+                brick.MoveMotor(rightMot, turnSpeed);
+                pause(turnTime*2);
                 brick.MoveMotor(leftMot+rightMot, 0);
-                dist = brick.UltrasonicDist(ultra);
-                if dist > 1
-                    brick.MoveMotor(leftMot+rightMot, 50);
-                    pause(2);
-                else
-                    angle = brick.GyroAngle(gyro);
-                    while abs(brick.GyroAngle(gyro))<abs(angle)+90
-                    brick.MoveMotor(leftMot, 25);
-                    brick.MoveMotor(rightMot, -25);
-                    end
+            dist = brick.UltrasonicDist(ultra);
+            if dist < 30 %if left is impossible, turn back
+                 %rotate left
+                    brick.MoveMotor(leftMot, -turnSpeed);
+                    brick.MoveMotor(rightMot, turnSpeed);
+                    pause(turnTime);
                     brick.MoveMotor(leftMot+rightMot, 0);
-                    brick.MoveMotor(leftMot+rightMot, 50);
-                    pause(2);
-                end
-            end            
+            end
         end
-        disp(rgb);
-        if rgb(1)>rgb(2)+rgb(3)+50
-            brick.MoveMotor(leftMot+rightMot, 0);
-            pause(5);
-            brick.MoveMotor('AD', 50);
-            pause(3);
-            brick.MoveMotor('AD', 0);
-        elseif rgb(1)+rgb(2)-rgb(3)>500
-            brick.MoveMotor('AD', 100);
-            pause(1.5);
-            brick.MoveMotor('AD', 0);
-            driving = false;
-        else
-            brick.MoveMotor('AD', 50);
-            pause(3);
-            brick.MoveMotor('AD', 0);
+    end
+    %go
+        brick.MoveMotor(leftMot+rightMot, moveSpeed);
+        pause(moveTime);
+        rgb = brick.ColorRGB(colSen);
+        if rgb(1)>rgb(2)+rgb(3) %if red's seen wait before moving forward, wait longer
+        pause(5);
         end
+        brick.MoveMotor(leftMot+rightMot, moveSpeed);
+        pause(moveTime);
+
+    rgb = brick.ColorRGB(colSen);
+    if(rgb(2) > rgb(1) + rgb(3) + 100) %if it's in the endzone (currently green), stop
+        driving = false;
+    end
+    if(rgb(1) + rgb(2) - rgb(3) > 350) %if it's in pickup area (currently yellow), pickup
+        pickup=true;
+        while pickup
+            disp("pickup sequence");
+            pickup=false;
+        end
+    end
 end
+
 dropoff=true;
 while dropoff
-    disp(dropoff);
+    disp("dropoff sequence");
     dropoff=false;
 end
+
+brick.MoveMotor(leftMot+rightMot, 0);
 DisconnectBrick(brick);
 clear brick;
