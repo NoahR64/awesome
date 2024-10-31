73cba745074c99de2fd79d0be5e17c67087181e4
NoahR64 <nroth8@asu.edu>
2024-10-30 18:54:11 -0700

    Add files via upload

--- cool.m
+++ cool.m
@@ -5,34 +5,41 @@ leftMot = 'D';
 rightMot = 'A';
 colSen = 3;
 gyro = 4;
-moveSpeed = 50;
-moveTime = 1.36;
-turnSpeed = 75;
-turnTime = 0.432;
+moveSpeed = 62.5;
+moveTime = 1.4;
+turnSpeed = 79;
+turnTime = 0.43;
 
 brick.SetColorMode(colSen, 4);
 
 driving = true;
 while driving
     pause(0.5);
-
-    %determine the direction to go
-    dist = brick.UltrasonicDist(ultra);
-    if dist < 30 %if forward is impossible, look right
-        %rotate right
+    %determine the direction to go right -> forward -> left -> back
+    %rotate right
             brick.MoveMotor(leftMot, turnSpeed);
             brick.MoveMotor(rightMot, -turnSpeed);
             pause(turnTime);
             brick.MoveMotor(leftMot+rightMot, 0);
+            pause(0.5)
+    dist = brick.UltrasonicDist(ultra);
+    if dist < 40 %if right is impossible, look forward
+        %rotate left
+            brick.MoveMotor(leftMot, -turnSpeed+3);
+            brick.MoveMotor(rightMot, turnSpeed-3);
+            pause(turnTime);
+            brick.MoveMotor(leftMot+rightMot, 0);
+        pause(0.5);
         dist = brick.UltrasonicDist(ultra);
-        if dist < 30 %if right is impossible, look left
-            %rotate left x2
-                brick.MoveMotor(leftMot, -turnSpeed);
-                brick.MoveMotor(rightMot, turnSpeed);
-                pause(turnTime*2);
+        if dist < 40 %if forward is impossible, look left
+            %rotate left
+                brick.MoveMotor(leftMot, -turnSpeed+6);
+                brick.MoveMotor(rightMot, turnSpeed-6);
+                pause(turnTime);
                 brick.MoveMotor(leftMot+rightMot, 0);
+            pause(0.5);
             dist = brick.UltrasonicDist(ultra);
-            if dist < 30 %if left is impossible, turn back
+            if dist < 40 %if left is impossible, turn back
                  %rotate left
                     brick.MoveMotor(leftMot, -turnSpeed);
                     brick.MoveMotor(rightMot, turnSpeed);
@@ -41,16 +48,19 @@ while driving
             end
         end
     end
-    %go
+    
+    %move to next square (checking for red in between)
         brick.MoveMotor(leftMot+rightMot, moveSpeed);
         pause(moveTime);
-        rgb = brick.ColorRGB(colSen);
-        if rgb(1)>rgb(2)+rgb(3) %if red's seen wait before moving forward, wait longer
+        brick.MoveMotor(leftMot+rightMot, 0);
+        pause(0.25);
+        if rgb(1)>rgb(2)+rgb(3) %if red's seen, wait before moving forward
         pause(5);
         end
         brick.MoveMotor(leftMot+rightMot, moveSpeed);
         pause(moveTime);
-
+        brick.MoveMotor(leftMot+rightMot, 0);
+    
     rgb = brick.ColorRGB(colSen);
     if(rgb(2) > rgb(1) + rgb(3) + 100) %if it's in the endzone (currently green), stop
         driving = false;
