e3cee9a4ef7f2457973fed2a50e1c74ffa30f5ab
NoahR64 <nroth8@asu.edu>
2024-11-20 19:09:53 -0700

    Add files via upload

--- cool.m
+++ cool.m
@@ -3,15 +3,19 @@ brick = ConnectBrick('GROUP8');
 ultra = 1;
 leftMot = 'D';
 rightMot = 'A';
+grabMot = 'C';
 colSen = 3;
 gyro = 4;
 moveSpeed = 63;
 moveTime = 1.45;
 turnSpeed = 80;
 turnTime = 0.43;
+grabSpeed = 15;
 correcter = 1.024;
 
+
 brick.SetColorMode(colSen, 2);
+InitKeyboard();
 
 driving = true;
 while driving
@@ -66,37 +70,74 @@ while driving
         brick.MoveMotor(leftMot+rightMot, 0);
     
     rgb = brick.ColorCode(colSen);
-    if(rgb == 3) %if it's in dropoff area (currently green), dropoff
+    if(rgb == 4) %if it's in dropoff area (currently yellow), dropoff
         dropoff=true;
         while dropoff
-            disp("dropoff sequence");
-            brick.playTone(100, 300, 250);
-            pause(.25);
-            brick.playTone(100, 100, 250);
-            pause(.25);
-            brick.playTone(100, 300, 250);
-            pause(.25);
-            dropoff=false;
+            %manual controls
+            pause(0.1);
+            switch key
+                case 'uparrow'
+                    brick.MoveMotor(rightMot, 50);
+                    brick.MoveMotor(leftMot, 50);
+                    pause(0.1);
+                case 'downarrow'
+                    brick.MoveMotor(leftMot, -50);
+                    brick.MoveMotor(rightMot, -50);
+                    pause(0.1);
+                case 'leftarrow'
+                    brick.MoveMotor(leftMot, -40);
+                    brick.MoveMotor(rightMot, 40);
+                    pause(0.1);
+                case 'rightarrow'
+                    brick.MoveMotor(leftMot, grabSpeed);
+                    brick.MoveMotor(rightMot, -grabSpeed);
+                    pause(0.1);
+                case 'z' %up
+                    brick.MoveMotor(grabMot, grabSpeed);
+                case 'x'%down
+                    brick.MoveMotor(grabMot, -grabSpeed);
+                case 'space'
+                    dropoff=false;
+                case 0
+                    brick.MoveMotor(leftMot+rightMot+grabMot, 0); 
+            end
         end
     end
-    if(rgb == 4) %if it's in pickup area (currently yellow), pickup
+    if(rgb == 3) %if it's in pickup area (currently green), pickup
         pickup=true;
         while pickup
-            disp("pickup sequence");
-            brick.playTone(100, 100, 250);
-            pause(.25);
-            brick.playTone(100, 300, 250);
-            pause(.25);
-            brick.playTone(100, 100, 250);
-            pause(.25);
-            brick.playTone(100, 300, 250);
-            pause(.25);
-            pickup=false;
+            %manual controls
+            pause(0.1);
+            switch key
+                case 'uparrow'
+                    brick.MoveMotor(leftMot, 20);
+                    brick.MoveMotor(rightMot, 20);
+                    pause(0.1);
+                case 'downarrow'
+                    brick.MoveMotor(leftMot, -20);
+                    brick.MoveMotor(rightMot, -20);
+                    pause(0.1);
+                case 'leftarrow'
+                    brick.MoveMotor(leftMot, -20);
+                    brick.MoveMotor(rightMot, 20);
+                    pause(0.1);
+                case 'rightarrow'
+                    brick.MoveMotor(leftMot, 20);
+                    brick.MoveMotor(rightMot, -20);
+                    pause(0.1);
+                case 'z'
+                    brick.MoveMotor(grabMot, grabSpeed);
+                case 'x'
+                    brick.MoveMotor(grabMot, -grabSpeed);
+                case 'space'
+                    pickup=false;
+                case 0
+                    brick.MoveMotor(leftMot+rightMot+grabMot, 0); 
+            end
         end
     end
     if(rgb == 2) %if it's in end area (currently blue), stop
-        pause(0.5);
-        disp("ending sequence");
+        pause(0.25);
         brick.playTone(100, 300, 250);
         pause(.25);
         brick.playTone(100, 100, 250);
